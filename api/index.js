479d2ab9dcb604fab5ac8ea436b39d281e25d6fe
Brijesh Savaj <brijeshsavaj@Brijeshs-MacBook-Pro.local>
2024-06-22 18:58:03 -0400

    Cors added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const app = express()
-const port = 3000
+const cors = require('cors');
 
+const app = express()
+// const port = 3000
+
+app.use(cors());
 app.use(bodyParser.json());
 
 
