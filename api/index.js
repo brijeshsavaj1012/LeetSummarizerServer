01a2a62b63fe7221d5583f45947d84a959960cdb
Brijesh Savaj <brijeshsavaj@Brijeshs-MacBook-Pro.local>
2024-06-25 16:22:14 -0400

    env file setup

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+
 
 
 // const corsOptions = {
@@ -12,10 +12,25 @@ const port = 3000;
 //   origin: ['https://leetcode.com/', 'http://localhost:80'] // Whitelist the domains you want to allow
 // };
 
+require('dotenv').config();
+// const base64String = fs.readFileSync('service_account_key_base64.txt', 'utf8');
+// const serviceAccountJSON = Buffer.from(base64String, 'base64').toString('utf-8');
+// const serviceAccount = JSON.parse(serviceAccountJSON);
 
-const base64String = fs.readFileSync('service_account_key_base64.txt', 'utf8');
-const serviceAccountJSON = Buffer.from(base64String, 'base64').toString('utf-8');
-const serviceAccount = JSON.parse(serviceAccountJSON);
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
+
+//const serviceAccount = require('./service_account_key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
